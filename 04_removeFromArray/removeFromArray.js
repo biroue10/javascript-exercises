07622ea338961dc8d9af513439893ac53a6f2006
BIROUE ISAAC <biroueisaac@gmail.com>
2023-06-23 18:13:04 +0100

    Second Commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(chaine, number) {
+    let index = chaine.indexOf(number)
+    let result = chaine.splice(index, 1)
+    return chaine
 };
 
 // Do not edit below this line
